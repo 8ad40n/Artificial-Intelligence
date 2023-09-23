301c8b8900286bf033da8b35f1f09ca01bb9d17c
Badhon Nath Joy <8ad40n@gmail.com>
2023-09-24 00:17:10 +0600

    added

---
+++ BASIC PYTHON/main.py
@@ -0,0 +1,24 @@
+print("Hello World")
+name="Badhon Nath Joy"
+age= 22
+is_adult= True
+#This is a comment
+print("Name: ",name)
+print("Age: ",age)
+print("Adult: ",is_adult)
+
+name1= input("Enter your name: ")
+print ("Hello",name1)
+age1=input("Enter age:")
+print("Age:",float(age1))
+new_age=int(age1)+2
+print("New age:",new_age)
+
+
+print(name.lower())
+print(name.upper())
+print(name.find('a'))
+print(name.replace("Joy","Jooy"))
+
+print("B" in name)
+print("Z" in name)
