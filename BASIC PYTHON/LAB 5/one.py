0fbf1a5fd21a24f83acf1327f5ca7d527507e1fb
Badhon Nath Joy <8ad40n@gmail.com>
2023-10-17 16:49:54 +0600

    Added

--- BASIC PYTHON/LAB 5/one.py
+++ BASIC PYTHON/LAB 5/one.py
@@ -1,15 +1,32 @@
-def selection_sort(products):
-    n = len(products)
-    for i in range(n):
-        min_index = i
-        for j in range(i + 1, n):
-            if products[j][1] < products[min_index][1]:
-                min_index = j
-        products[i], products[min_index] = products[min_index], products[i]
+def distribute_products(products):
+    shelves = [[] for _ in range(5)]  # Create 5 shelves
+
+    for product in products:
+      if product[1] <= 20:
+        shelves[0].append(product)
+      elif product[1] <= 40:
+        shelves[1].append(product)
+      elif product[1] <= 60:
+        shelves[2].append(product)
+      elif product[1] <= 80:
+        shelves[3].append(product)
+      else:
+        shelves[4].append(product)
+
+    return shelves
 
 
-products = [("Dairy Milk", 50), ("Kitkat", 75), ("Ice cream", 60), ("Mountain Dew", 20)]
-selection_sort(products)
+def display_shelves(shelves):
+  for i, shelf in enumerate(shelves):
+    print(f"Shelf {i + 1}:")
+    for product in shelf:
+      print(f"Product: {product[0]}, Price: {product[1]}")
+    print("-" * 30)
 
-for product in products:
-    print(f"{product[0]} - BDT {product[1]}")
+
+if __name__ == "__main__":
+  products = [("Dairy Milk", 50), ("Kitkat", 75), ("Ice cream", 60), ("Mountain Dew", 20)]
+
+  shelves = distribute_products(products)
+  display_shelves(shelves)
+ 
